0ab443a33fb0edde2489ef25feb6f067b2a3c684
noraeb <n.blomaard@gmail.com>
2026-01-30 10:31:06 +0000

    Deploying to deploy from @ com-pas/oscd-bay-template-editor@c02d9ff43f893b3f3ca7dce361ddc312a5e14b77 🚀

--- bay-template-editor.d.ts
+++ bay-template-editor.d.ts
@@ -1,13 +1,9 @@
 import { LitElement } from 'lit';
-declare const BayTemplatePlugin_base: typeof LitElement & import("@open-wc/scoped-elements/lit-element.js").ScopedElementsHostConstructor;
 /** An editor [[`plugin`]] for creating bay templates using single line diagrams */
-export default class BayTemplatePlugin extends BayTemplatePlugin_base {
-    static scopedElements: {};
-    /** The document being edited as provided to plugins by [[`OpenSCD`]]. */
+export default class BayTemplatePlugin extends LitElement {
     doc: XMLDocument;
-    /** SCL change indicator */
     editCount: number;
+    docVersion: number;
     render(): import("lit-html").TemplateResult<1>;
     static styles: import("lit").CSSResult;
 }
-export {};
