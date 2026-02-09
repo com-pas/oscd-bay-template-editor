84879850a2dcf3d127b4facc2a390ff47725b506
noraeb <n.blomaard@gmail.com>
2026-02-09 10:07:55 +0000

    Deploying to deploy from @ com-pas/oscd-bay-template-editor@04fea9519398fe04ffc86fd916e7f1f7c501c30b 🚀

--- bay-template-editor.d.ts
+++ bay-template-editor.d.ts
@@ -1,9 +1,28 @@
 import { LitElement } from 'lit';
+import '@material/mwc-fab';
+import type { SldEditor } from '@omicronenergy/oscd-editor-sld/dist/sld-editor.js';
+import type { IconButtonToggle } from '@material/mwc-icon-button-toggle';
+import '@omicronenergy/oscd-editor-sld/dist/sld-editor.js';
 /** An editor [[`plugin`]] for creating bay templates using single line diagrams */
 export default class BayTemplatePlugin extends LitElement {
-    doc: XMLDocument;
+    doc?: XMLDocument;
     editCount: number;
-    docVersion: number;
+    gridSize: number;
+    sldEditor?: SldEditor;
+    get showLabels(): boolean;
+    labelToggle?: IconButtonToggle;
+    inAction: boolean;
+    templateElements: Record<string, Element>;
+    nsp: string;
+    connectedCallback(): void;
+    disconnectedCallback(): void;
+    private preprocessEdits;
+    updated(changedProperties: Map<string, any>): void;
+    zoomIn(): void;
+    zoomOut(): void;
+    startPlacing(element: Element | undefined): void;
+    reset(): void;
+    insertSubstation(): void;
     render(): import("lit-html").TemplateResult<1>;
-    static styles: import("lit").CSSResult;
+    static styles: import("lit").CSSResult[];
 }
