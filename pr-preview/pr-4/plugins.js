156cf2c3ec24b4cbb2f0a509f054f8364e00c7b2
noraeb <noraeb@users.noreply.github.com>
2026-02-10 13:26:52 +0000

    Deploy preview for PR 4 🛫

---
+++ pr-preview/pr-4/plugins.js
@@ -0,0 +1,33 @@
+export const plugins = {
+  menu: [
+    {
+      name: 'Open File',
+      translations: { de: 'Datei öffnen' },
+      icon: 'folder_open',
+      active: true,
+      src: 'https://openscd.github.io/oscd-open/oscd-open.js',
+    },
+    {
+      name: 'Save File',
+      translations: { de: 'Datei speichern' },
+      icon: 'save',
+      active: true,
+      src: 'https://openscd.github.io/oscd-save/oscd-save.js',
+    },
+    {
+      name: 'Wizarding',
+      icon: 'edit',
+      active: true,
+      src: 'https://openenergytools.github.io/scl-editor/plugins/scl-wizarding/scl-wizarding.js',
+    },
+  ],
+  editor: [
+    {
+      name: 'Bay Template Editor',
+      translations: { de: 'Bay Template Editor' },
+      icon: 'add_box',
+      active: true,
+      src: '/dist/bay-template-editor.js',
+    },
+  ],
+};
