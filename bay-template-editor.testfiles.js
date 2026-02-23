28b140bf559c99c105710b149b28c5ca3e34f44f
noraeb <n.blomaard@gmail.com>
2026-02-23 16:04:28 +0000

    Deploying to deploy from @ com-pas/oscd-bay-template-editor@be524f505137b567ff3c7caf05b8b95fb88e37a7 🚀

---
+++ bay-template-editor.testfiles.js
@@ -0,0 +1,37 @@
+export const emptyDoc = `<?xml version="1.0" encoding="UTF-8"?>
+<SCL xmlns="http://www.iec.ch/61850/2003/SCL" version="2007" revision="B">
+</SCL>`;
+export const docWithSubstation = `<?xml version="1.0" encoding="UTF-8"?>
+<SCL xmlns="http://www.iec.ch/61850/2003/SCL" version="2007" revision="B">
+  <Substation name="S1" />
+</SCL>`;
+export const docWithVoltageLevel = `<?xml version="1.0" encoding="UTF-8"?>
+<SCL xmlns="http://www.iec.ch/61850/2003/SCL" version="2007" revision="B">
+  <Substation name="S1">
+    <VoltageLevel name="V1" />
+  </Substation>
+</SCL>`;
+export const docWithBay = `<?xml version="1.0" encoding="UTF-8"?>
+<SCL xmlns="http://www.iec.ch/61850/2003/SCL" version="2007" revision="B">
+  <Substation name="S1">
+    <VoltageLevel name="V1">
+      <Bay name="B1" />
+    </VoltageLevel>
+  </Substation>
+</SCL>`;
+export const docWithBusBarBay = `<?xml version="1.0" encoding="UTF-8"?>
+<SCL xmlns="http://www.iec.ch/61850/2003/SCL"
+  xmlns:eosld="https://openscd.org/SCL/SSD/SLD/v0" version="2007" revision="B">
+  <Substation name="S1">
+    <VoltageLevel name="110kV">
+      <Bay name="BB1">
+        <Private type="OpenSCD-SLD-Layout">
+          <eosld:SLDAttributes eosld:x="2" eosld:y="2" eosld:w="20" eosld:h="3"/>
+          <Section name="BUS1" eosld:bus="true" eosld:x="2" eosld:y="2"
+                   eosld:w="18" eosld:h="2"/>
+        </Private>
+      </Bay>
+    </VoltageLevel>
+  </Substation>
+</SCL>`;
+//# sourceMappingURL=bay-template-editor.testfiles.js.map
--- util.d.ts
+++ util.d.ts
@@ -1,3 +1,4 @@
+import { EditV2 } from '@openscd/oscd-api';
 export declare const privType = "OpenSCD-SLD-Layout";
 export declare const sldNs = "https://openscd.org/SCL/SSD/SLD/v0";
 export declare const xmlnsNs = "http://www.w3.org/2000/xmlns/";
@@ -10,6 +11,7 @@ export declare const ringedEqTypes: Set<string>;
 export declare const singleTerminal: Set<string>;
 export declare function setSLDAttributes(element: Element, nsPrefix: string, values: Record<string, string>): void;
 export declare function getSLDAttributes(element: Element, key: string): string | null;
+export declare function updateSLDAttributes(element: Element, nsPrefix: string, values: Partial<Record<string, string | null>>): EditV2;
 export declare function busSections(element: Element): Element[];
 export declare function isBusBar(element: Element): boolean;
 export declare function makeBusBar(doc: XMLDocument, nsp: string): Element;
