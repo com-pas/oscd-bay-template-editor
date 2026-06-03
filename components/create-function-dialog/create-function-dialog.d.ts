dd023d1df795ea4ba063961cdcbc1f86bdd40832
noraeb <n.blomaard@gmail.com>
2026-06-03 11:53:33 +0000

    Deploying to gh-pages from @ com-pas/oscd-bay-template-editor@208d03f6a8fbc4e8a7fbb9b894097d338dc371ca 🚀

--- components/create-function-dialog/create-function-dialog.d.ts
+++ components/create-function-dialog/create-function-dialog.d.ts
@@ -3,6 +3,15 @@ import { OscdDialog } from '@omicronenergy/oscd-ui/dialog/OscdDialog.js';
 import { OscdFilledButton } from '@omicronenergy/oscd-ui/button/OscdFilledButton.js';
 import { OscdFilledTextField } from '@omicronenergy/oscd-ui/textfield/OscdFilledTextField.js';
 import { OscdSclTextField } from '@omicronenergy/oscd-ui/scl-textfield/OscdSclTextField.js';
+import { OscdIcon } from '@omicronenergy/oscd-ui/icon/OscdIcon.js';
+import { OscdIconButton } from '@omicronenergy/oscd-ui/iconbutton/OscdIconButton.js';
+import { OscdDivider } from '@omicronenergy/oscd-ui/divider/OscdDivider.js';
+import { OscdList } from '@omicronenergy/oscd-ui/list/OscdList.js';
+import { OscdListItem } from '@omicronenergy/oscd-ui/list/OscdListItem.js';
+export declare enum CreateFunctionDialogStep {
+    FunctionAttributes = "function-attributes",
+    FunctionContent = "function-content"
+}
 declare const CreateFunctionDialog_base: typeof LitElement & import("@open-wc/scoped-elements/lit-element.js").ScopedElementsHostConstructor;
 export declare class CreateFunctionDialog extends CreateFunctionDialog_base {
     static get scopedElements(): {
@@ -10,23 +19,35 @@ export declare class CreateFunctionDialog extends CreateFunctionDialog_base {
         'oscd-filled-button': typeof OscdFilledButton;
         'oscd-filled-text-field': typeof OscdFilledTextField;
         'oscd-scl-text-field': typeof OscdSclTextField;
+        'oscd-icon': typeof OscdIcon;
+        'oscd-icon-button': typeof OscdIconButton;
+        'oscd-divider': typeof OscdDivider;
+        'oscd-list': typeof OscdList;
+        'oscd-list-item': typeof OscdListItem;
     };
     parent: Element | null;
+    selectedElementName: string;
+    selectedElementType: string;
     dialog: OscdDialog;
     nameField: OscdFilledTextField;
     descriptionField: OscdSclTextField;
     typeField: OscdSclTextField;
     name: string;
-    description: null;
-    type: null;
+    description: string | null;
+    type: string | null;
+    step: CreateFunctionDialogStep;
     private formGroup;
+    private shouldEmitCancel;
     show(): void;
     close(): void;
-    cancel(): void;
     reset(): void;
-    private nameTakenValidator;
-    private handleSubmit;
+    private handleClosed;
+    private readonly nameTakenValidator;
+    private handleNext;
+    private handleSave;
+    renderFunctionAttrs(): import("lit-html").TemplateResult<1>;
+    renderFunctionContent(): import("lit-html").TemplateResult<1>;
     render(): import("lit-html").TemplateResult<1>;
-    static styles: import("lit").CSSResult;
+    static readonly styles: import("lit").CSSResult;
 }
 export {};
