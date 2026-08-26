acc3fd8a7ab66e4c2af161b2550bb7920bbd9d8c
noraeb <noraeb@users.noreply.github.com>
2026-08-26 07:50:27 +0000

    Deploy preview for PR 29 🛫

---
+++ pr-preview/pr-29/components/function-link-dialog/function-link-dialog.d.ts
@@ -0,0 +1,60 @@
+import { LitElement } from 'lit';
+import { OscdDialog } from '@omicronenergy/oscd-ui/dialog/OscdDialog.js';
+import { OscdFilledButton } from '@omicronenergy/oscd-ui/button/OscdFilledButton.js';
+import { OscdFilledTextField } from '@omicronenergy/oscd-ui/textfield/OscdFilledTextField.js';
+import { OscdIcon } from '@omicronenergy/oscd-ui/icon/OscdIcon.js';
+import { OscdIconButton } from '@omicronenergy/oscd-ui/iconbutton/OscdIconButton.js';
+import { OscdList } from '@omicronenergy/oscd-ui/list/OscdList.js';
+import { OscdListItem } from '@omicronenergy/oscd-ui/list/OscdListItem.js';
+import { OscdFilledSelect } from '@omicronenergy/oscd-ui/select/OscdFilledSelect.js';
+import { OscdSelectOption } from '@omicronenergy/oscd-ui/select/OscdSelectOption.js';
+import { type LinkService, type ObjectReferenceItem } from './object-references.js';
+export interface CreateFunctionLinkEventDetail {
+    service: LinkService;
+    selectedReferences: ObjectReferenceItem[];
+}
+declare const FunctionLinkDialog_base: typeof LitElement & import("@open-wc/dedupe-mixin").Constructor<import("@open-wc/scoped-elements/types.js").ScopedElementsHost> & import("@open-wc/scoped-elements/types.js").ScopedElementsHostConstructor;
+export declare class FunctionLinkDialog extends FunctionLinkDialog_base {
+    static get scopedElements(): {
+        'oscd-dialog': typeof OscdDialog;
+        'oscd-filled-button': typeof OscdFilledButton;
+        'oscd-filled-text-field': typeof OscdFilledTextField;
+        'oscd-icon': typeof OscdIcon;
+        'oscd-icon-button': typeof OscdIconButton;
+        'oscd-list': typeof OscdList;
+        'oscd-list-item': typeof OscdListItem;
+        'oscd-filled-select': typeof OscdFilledSelect;
+        'oscd-select-option': typeof OscdSelectOption;
+    };
+    private readonly dialog;
+    sourceFunctionName: string;
+    sourceFunctionPath: string;
+    private objectReferenceGroups;
+    private filterQuery;
+    private selectedService;
+    private selectedReferenceIds;
+    private readonly boundHandleDocumentKeydown;
+    get open(): boolean;
+    show(): void;
+    showForSourceFunction(sourceFunction: Element, doc: Document): void;
+    close(): void;
+    disconnectedCallback(): void;
+    private resetSelectionState;
+    private get filteredGroups();
+    private get selectedReferences();
+    private get canConnect();
+    private handleSearchInput;
+    private clearSearchFilter;
+    private handleServiceChange;
+    private handleToggleReference;
+    private handleConnect;
+    private dispatchCloseEvent;
+    private handleDialogClosed;
+    private handleCancel;
+    private handleDocumentKeydown;
+    private renderReferenceRow;
+    private renderReferenceList;
+    render(): import("lit-html").TemplateResult<1>;
+    static styles: import("lit").CSSResult;
+}
+export {};
