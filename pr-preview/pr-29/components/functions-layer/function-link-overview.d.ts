8db1cf024bc02c316f246bbfe5f3292e1bc7885e
noraeb <noraeb@users.noreply.github.com>
2026-09-02 10:10:02 +0000

    Deploy preview for PR 29 🛫

--- pr-preview/pr-29/components/functions-layer/function-link-overview.d.ts
+++ pr-preview/pr-29/components/functions-layer/function-link-overview.d.ts
@@ -1,12 +1,14 @@
 import { LitElement, nothing } from 'lit';
 import { OscdIcon } from '@omicronenergy/oscd-ui/icon/OscdIcon.js';
 import { OscdFilledButton } from '@omicronenergy/oscd-ui/button/OscdFilledButton.js';
+import { OscdIconButton } from '@omicronenergy/oscd-ui/iconbutton/OscdIconButton.js';
 import { type FunctionLink } from './function-links.js';
 declare const FunctionLinkOverview_base: typeof LitElement & import("@open-wc/dedupe-mixin").Constructor<import("@open-wc/scoped-elements/types.js").ScopedElementsHost> & import("@open-wc/scoped-elements/types.js").ScopedElementsHostConstructor;
 export declare class FunctionLinkOverview extends FunctionLinkOverview_base {
     static get scopedElements(): {
         'oscd-icon': typeof OscdIcon;
         'oscd-filled-button': typeof OscdFilledButton;
+        'oscd-icon-button': typeof OscdIconButton;
     };
     selectedLink?: FunctionLink;
     expandedDetails: boolean;
