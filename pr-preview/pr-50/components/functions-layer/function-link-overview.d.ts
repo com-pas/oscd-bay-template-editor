1ab6409e8851634e9ba306cd616efc69de27c419
noraeb <noraeb@users.noreply.github.com>
2026-09-21 11:43:50 +0000

    Deploy preview for PR 50 🛫

--- pr-preview/pr-50/components/functions-layer/function-link-overview.d.ts
+++ pr-preview/pr-50/components/functions-layer/function-link-overview.d.ts
@@ -19,12 +19,16 @@ export declare class FunctionLinkOverview extends FunctionLinkOverview_base {
     overviewTop?: number;
     private pendingDeleteSelectedLink;
     private pendingRemovedSourceRefKeys;
+    private infoSourceRef?;
     protected willUpdate(changedProperties: PropertyValues): void;
     private resetPendingChanges;
     private deleteLink;
     private deleteSourceRef;
     private closeOverview;
     private saveOverview;
+    private openSourceRefInfo;
+    private closeSourceRefInfo;
+    private renderInfoPane;
     private hasPendingSourceRefDeletion;
     private getVisibleSourceRefs;
     private renderDeleteWarning;
